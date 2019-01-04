82fc1aed9ab6993e4581d1b3be25cc5209784cdc
William Taylor <william.taylor.1707@gmail.com>
2019-01-04 01:12:18 +0000

    Adding system screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { AgGridModule } from 'ag-grid-angular';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './component/app/app.component';
-import { HomeComponent } from './component/home/home.component';
 import { QueryComponent } from './component/query/query.component';
 import { SystemComponent } from './component/system/system.component';
 import { AuditComponent } from './component/audit/audit.component';
@@ -21,7 +23,7 @@ let routes: Routes = [
   { path: 'query', component: QueryComponent },
   { path: 'audit', component: AuditComponent },
   { path: 'system', component: SystemComponent },
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/query', pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ]
 
@@ -30,15 +32,16 @@ let routes: Routes = [
     QueryComponent,
     AuditComponent,
     SystemComponent,
-    HomeComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
     BsDatepickerModule.forRoot(),
     BsDropdownModule.forRoot(),
+    ProgressbarModule.forRoot(),
     TypeaheadModule.forRoot(),
     ModalModule.forRoot(),
     TabsModule.forRoot(),
--- src/app/component/system/system.component.ts
+++ src/app/component/system/system.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'system-view',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./system.component.css']
 })
 export class SystemComponent {
-    
+    rowData = [];
+    columnDefs = [
+        { headerName: 'Feed', field: 'feed' },
+        { headerName: 'Jobs', field: 'jobs' },
+        { headerName: 'Success', field: 'success' },
+        { headerName: 'Waiting', field: 'waiting' },
+        { headerName: 'Failed', field: 'failed' },
+    ];
+
+    ngOnInit() {
+        for(let i = 0; i < 25; i++) {
+            this.rowData.push({
+                feed: 'Accounts',
+                jobs: 20,
+                success: 5,
+                waiting: 10,
+                failed: 5
+            })
+        }
+    }
 }
